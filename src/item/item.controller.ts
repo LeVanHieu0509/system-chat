299a08ff3317ab24161f62a2478b0d102f34e103
LeVanHieu0509 <leVanHieu.huflit@gmail.com>
2023-08-25 09:25:50 +0700

    init nest js

---
+++ src/item/item.controller.ts
@@ -0,0 +1,18 @@
+import { Controller, Get } from '@nestjs/common';
+import { ItemService } from './item.service';
+
+@Controller('item')
+export class ItemController {
+    constructor(private serv: ItemService) { }
+
+    @Get()
+    public async getAll(): Promise<ItemDTO[]> {
+        return await this.serv.getAll()
+    }
+
+    @Post()
+    public async post(@User() user: User, @Body() dto: ItemDTO): Promise<ItemDTO> {
+        return this.serv.create(dto, user);
+    }
+
+}   
